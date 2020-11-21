03f6222158e9d53d9c0411a385e3d94a9cd0aa17
dacoonkr <dacoon0621@gmail.com>
2020-11-21 13:48:14 +0900

    페이지 구성

---
+++ src/pages/index.tsx
@@ -0,0 +1,5 @@
+export { default as Main } from "./Main";
+export { default as Intro } from "./Intro";
+export { default as Member } from "./Member";
+export { default as Project } from "./Project";
+export { default as NotFound } from "./NotFound";
