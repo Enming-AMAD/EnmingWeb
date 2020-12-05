95dec48561918fa2321fc0aac34c64f5dc600f5b
dacoonkr <dacoon0621@gmail.com>
2020-12-05 20:57:22 +0900

    Update Main.tsx

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,8 +1,16 @@
 import React from "react";
 
+import "./stylesheet/Main.css"
+
+import logo from "../images/logobig.png"
+
 const Page = () => {
     return (
-        <div>Main Page</div>
+        <div className="slogans">
+            <div className="slogan topSlogan">게임을 만듭니다</div>
+            <img alt="" className="sloganImage" src={logo}></img>
+            <div className="slogan bottomSlogan">즐거움을 만듭니다</div>
+        </div>
     );
 };
 
